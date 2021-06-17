0df2f7d5a80092f0f2e3d848bfdb16e81b5d981f
Husain Ahmmed <husainahmmed1b@gmail.com>
2021-06-17 19:45:39 +0600

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-      <div>
+      <>
+        <Particles 
+          className="particle-canvas"
+          params={{
+            particles: {
+              number: {
+                value: 30,
+                density: {
+                  enable: true,
+                  value_area: 900
+                }
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 6,
+                  color: "#f9ab00"
+                }
+              }
+            }
+          }}
+        />
         <Navbar />
-      </div>
+        <Header />
+      </>
   );
 }
 
